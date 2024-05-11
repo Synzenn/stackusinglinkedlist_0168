53050551307d3ea8e3bdad3a45660323a4046fef
Synzenn <velriel42@gmail.com>
2024-05-11 11:44:05 +0700

    membuat kondisi else

--- Algo11mei/Algo11mei.cpp
+++ Algo11mei/Algo11mei.cpp
@@ -42,7 +42,13 @@ public:
 		if (top == NULL) {
 			cout << "List is empty." << endl;
 		}
-		
+		else {
+			Node* current = top;
+			while (current != NULL) {
+				cout << current->data << " " << endl;
+				current = current->next;
+			}
+			cout << endl;
 		}
 	}
 
